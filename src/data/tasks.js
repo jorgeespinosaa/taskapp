065113889e451dcf35e2850bda83ffaa46a31384
Jorge Espinosa Alonso <jespinosaweb@gmail.com>
2022-08-27 20:24:38 -0400

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+      id: 0,
+      title: "primera tarea",
+      description: "primera tarea",
+    },
+    {
+      id: 1,
+      title: "segunda tarea",
+      description: "segunda tarea",
+    },
+    {
+      id: 2,
+      title: "tercera tarea",
+      description: "tercera tarea",
+    },
+    {
+      id: 3,
+      title: "cuarta tarea",
+      description: "cuarta tarea",
+    },
+  ];
